01a5a5571bd22032e820591a53679c1459bf4b96
vfxnake <jazzjoe@gmail.com>
2023-01-08 22:02:38 -0500

    init for classes and visual studio support.

---
+++ Source/slash_snake/slash_snake.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "slash_snake.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, slash_snake, "slash_snake" );
